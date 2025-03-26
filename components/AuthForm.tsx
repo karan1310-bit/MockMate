a883dd92c90ee2f2f54e11f7c204c3dd673adb84
karan <karanbhati1310@gmail.com>
2025-03-26 09:16:56 +0530

    error in deploying

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -44,53 +44,59 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
-        if(type === 'sign-up'){
+      if (type === "sign-up") {
+        const { name, email, password } = data;
 
-          const { name, email, password } = values;
-          const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-          const result = await signUp({
-            uid: userCredentials.user.uid,
-            name: name!,
-            email,
-            password,
-          })
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
 
-          if(!result?.success){
-            toast.error(result?.message);
-            return;
-          }
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
 
-            toast.success('Account created successfully')
-            router.push("/sign-in");
-        }else{
-
-          const { email, password } = values;
-          const userCredentials = await signInWithEmailAndPassword(auth, email, password);
-
-          const idToken = await userCredentials.user.getIdToken();
-
-          if(!idToken){
-            toast.error('Failed to sign in')
-            return;
-          }
-
-          await signIn({
-            email, idToken
-          })
-
-            toast.success('Logged In successfully')
-            router.push("/");
+        if (!result.success) {
+          toast.error(result.message);
+          return;
         }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        router.push("/");
+      }
     } catch (error) {
-        console.log(error)
-        toast.error(`Error: ${error}`)
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
     }
-  }
+  };
 
   const isSignIn = type === "sign-in";
 
